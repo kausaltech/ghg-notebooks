0205bb54b2c0dae79624b12a6c90b5b8105f0a7d
Juha Yrjölä <juha.yrjola@iki.fi>
2019-03-11 16:09:15 +0200

    Update settings

--- settings.py
+++ settings.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+MUNICIPALITY_ID = os.getenv("MUNICIPALITY_ID")
+DATA_DIR = os.getenv("DATA_DIR") or os.path.dirname(os.path.abspath(__file__))
+
 QUILT_USER = os.getenv("QUILT_USER")
 FINGRID_API_KEY = os.getenv("FINGRID_API_KEY")
-INFLUXDB_DSN = os.getenv("INFLUXDB_DSN")
 POSTGRESQL_DSN = os.getenv("POSTGRESQL_DSN")
