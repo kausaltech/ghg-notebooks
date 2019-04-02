099eca3c4d27c5773d316d42589b4bdbe2b84eb7
Juha Yrjölä <juha.yrjola@iki.fi>
2019-04-02 23:33:24 +0300

    Start using PX in Ympäristötilastot data importer

--- data_import/ymparistotilasto.py
+++ data_import/ymparistotilasto.py
@@ -1,48 +1,81 @@
+import os
 import csv
+from datetime import datetime, timedelta
+
 import pandas as pd
 from collections import OrderedDict
 import quilt
+from pandas_pcaxis import PxParser
+
+from utils.quilt import update_node_from_pcaxis
 
 
-# get_pcaxis_file('E21_kaup_rakennus_tyypeittain', '../DATABASE/02_ENERGIA/4_KAUPUNGIN_OMISTAMAT/')
-def _parse_csv(path):
-    all_rows = []
-    with open(path, 'r', encoding='iso8859-1') as csvf:
-        reader = csv.reader(csvf, delimiter=';', quotechar='"')
-        title = next(reader)[0]
-        # Empty row
-        next(reader)
-        field_names = next(reader)
-        field_names[0] = 'Rakennustyyppi'
-        field_names[1] = 'Vuosi'
-        current_type = None
-        for row in reader:
-            if not len(row):
-                break
-            data = OrderedDict(list(zip(field_names, row)))
-            title = row[0].strip()
-            if title:
-                current_type = title
+def update_quilt(quilt_path):
+    import glob
+    import settings
+
+    CATEGORIES = {
+        'J': 'Jate',
+        'I': 'Ilmanlaatu',
+        'L': 'Liikenne',
+        'V': 'Vedet',
+        'M': 'Maankaytto',
+        'T': 'Ymparistotalous',
+        'E': 'Energia',
+    }
+
+    def upload_px_dataset(root_node, file):
+        fname = os.path.splitext(os.path.basename(file))[0].lower()
+        letter = fname[0]
+        if letter != 'e':
+            return
+
+        content = open(file, 'r', encoding='windows-1252').read()
+        parser = PxParser()
+        file = parser.parse(content)
+        now = datetime.now()
+
+        if 'last_updated' not in file.meta or (now - file.meta['last_updated']) > timedelta(days=2 * 365):
+            return
+
+        print(fname)
+        if root_node:
+            quilt_target = root_node
+        else:
+            quilt_target = quilt_path
+
+        node = update_node_from_pcaxis(quilt_target, fname, file)
+        return node
+
+    SKIP_FILES = ['A4_kopiopaperi.px']
+
+    data_dir = os.path.join(settings.DATA_DIR, 'ymp')
+    files = glob.glob('%s/*.px' % data_dir)
+    skip_until = None
+
+    root_node = None
+    for file in files:
+        if skip_until:
+            if skip_until not in file:
                 continue
+            skip_until = None
 
-            data['Rakennustyyppi'] = current_type
-            for key, val in data.items():
-                if key == 'Rakennustyyppi':
-                    continue
-                if key == 'Vuosi':
-                    data[key] = int(val)
-                    continue
-                if val in ('..', '-'):
-                    data[key] = pd.np.nan
-                else:
-                    data[key] = float(data[key])
-            all_rows.append(data)
+        skip = False
+        for sf in SKIP_FILES:
+            if sf in file:
+                skip = True
+                break
+        if skip:
+            continue
 
-    df = pd.DataFrame(all_rows).set_index('Rakennustyyppi')
-    return df
+        ret = upload_px_dataset(root_node, file)
+        if ret:
+            root_node = ret
+
+    assert root_node
+    quilt.build(quilt_path, root_node)
+    quilt.push(quilt_path, is_public=True)
 
 
 if __name__ == '__main__':
-    df = _parse_csv('ytdata/E21_kaup_rakennus_tyypeittain18121617144011544976023865s.csv')
-    quilt.build('jyrjola/ymparistotilastot/e21_kaup_rakennus_tyypeittain', df)
-    quilt.push('jyrjola/ymparistotilastot/e21_kaup_rakennus_tyypeittain', is_public=True)
+    update_quilt('jyrjola/ymparistotilastot')
