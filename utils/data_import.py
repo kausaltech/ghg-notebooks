7fee945b9e451854e2fc2e64699305d83db4caeb
Juha Yrjölä <juha.yrjola@iki.fi>
2020-06-22 12:19:20 +0300

    Bug fixes for find_data_start

--- utils/data_import.py
+++ utils/data_import.py
@@ -4,24 +4,30 @@ from datetime import timedelta, date
 MIN_DATE = date(1950, 1, 1)
 
 
-def find_data_start(check_func, start_date=None):
+def find_data_start(check_func, max_date=None, min_date=None):
     """Bisect date ranges to find the first date with valid data"""
-    td = timedelta(days=1 << 12)
+    if not max_date:
+        max_date = date.today()
+
+    if min_date:
+        td = max_date - min_date
+    else:
+        td = timedelta(days=1 << 12)
+        min_date = MIN_DATE
     found_bad = False
     last_good = None
     last_bad = None
 
-    if not start_date:
-        start_date = date.today()
-
-    check_date = start_date - td
+    check_date = max_date - td
     last_bad = check_date
-    last_good = start_date
+    last_good = max_date
 
+    td /= 2
     while (last_good - last_bad) != timedelta(days=1):
         if td < timedelta(days=1):
             td = timedelta(days=1)
-        assert check_date >= MIN_DATE
+        assert check_date <= max_date
+        assert check_date >= min_date
         is_ok = check_func(check_date)
         if is_ok:
             last_good = check_date
