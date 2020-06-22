a2215794b92c42667c0a404245ab25695c18d37c
Juha Yrjölä <juha.yrjola@iki.fi>
2020-06-22 12:19:02 +0300

    Update settings

--- settings.py
+++ settings.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-MUNICIPALITY_ID = os.getenv("MUNICIPALITY_ID")
+MUNICIPALITY_ID = os.getenv("MUNICIPALITY_ID") or "091"  # default to Helsinki
 DATA_DIR = os.getenv("DATA_DIR") or os.path.dirname(os.path.abspath(__file__))
 
 QUILT_USER = os.getenv("QUILT_USER")
 FINGRID_API_KEY = os.getenv("FINGRID_API_KEY")
 POSTGRESQL_DSN = os.getenv("POSTGRESQL_DSN")
+NUUKA_API_BASE_URL = os.getenv("NUUKA_API_BASE_URL")
+NUUKA_API_KEY = os.getenv("NUUKA_API_KEY")
