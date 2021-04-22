4e5005187eaa11c429aad62e9640d29f5d5d4f68
Juha Yrjölä <juha.yrjola@iki.fi>
2021-04-22 21:57:06 +0300

    Make dvc stuff work better

--- settings.py
+++ settings.py
@@ -15,3 +15,6 @@ NUUKA_API_KEY = os.getenv("NUUKA_API_KEY")
 
 MML_API_KEY = os.getenv('MML_API_KEY')
 FMI_API_KEY = os.getenv('FMI_API_KEY')
+
+DVC_PANDAS_REPOSITORY = os.getenv('DVC_PANDAS_REPOSITORY', 'git@github.com:kausaltech/dvctest.git')
+DVC_PANDAS_DVC_REMOTE = os.getenv('DVC_PANDAS_DVC_REMOTE', 'kausal-s3')
